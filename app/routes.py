447fcbc521a260b855e9cb7c1861849fcddbee7f
Alejandro Peña <ja.penat1@uniandes.edu.co>
2024-09-26 22:14:57 -0500

    agregar ruta para crear y consultar facturas

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,23 @@
-from app import app
+from app import app, db
+from .models import Factura
+import datetime
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    facturas = Factura.query.all()
+    return [factura.id for factura in facturas]
+
+@app.route('/crear_factura', methods=['POST'])
+def crear_factura():
+    factura = Factura(
+        usuario_id=1,
+        nombre='Factura 1',
+        monto=100,
+        detalle='Detalle de la factura',
+        estado='pendiente',
+        fecha=datetime.datetime.now()
+    )
+    db.session.add(factura)
+    db.session.commit()
+    return f'Factura creada {factura.id}'
